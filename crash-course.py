35cd0097083b852d980555c74ea66633075bea29
Vibhu Vasanth <vibhuvasanth@gmail.com>
2026-09-08 18:22:21 +0000

    My first commit

---
+++ crash-course.py
@@ -0,0 +1,53 @@
+#comment
+
+
+# here 
+# is 
+# a 
+# comment 
+#to make lines of words that are not code, just highlight, then press control and the press forward slash
+
+print("Hello World!")
+
+#Variable Declarations:
+
+a = 4 # This is a integer 
+b = 5.5 # This is a float 
+c = "CSAEA" # This is a string 
+d = False # This is a boolean(VERY IMPORTANT NAMES AFER GEORGE BOOL)
+
+
+#These 4 data types you will need for the AP EXAM(VERY IMPORTANT!!!!!!)
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+#or you can do 
+
+print(a,b,c,d)
+
+#operators
+# These include, +,-,/,* and then you know modulus %, ** exponent, //
+
+#compound operators
+#+=, -=, /=
+
+e = 10 % 5
+
+print(e)
+
+e += 7
+
+print(e)
+
+# f - string- creates a dynamic string, mixes diffrent data types into a one sentence or string 
+
+print(f"e is equal to {e} ")
+
+e -= 7
+
+e+=12
+
+print(f"e is NOW equal to {e}")
